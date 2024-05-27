5a4efab327904ece998de00e5b11ae2a5e10c0a1
Junnos <junnos.dev@gmail.com>
2024-05-27 12:40:43 +0900

    Project Setup

---
+++ converter/__main__.py
@@ -0,0 +1,9 @@
+from .convert import Converter
+
+
+def main():
+    converter = Converter()
+
+
+if __name__ == "__main__":
+    main()
---
+++ converter/convert.py
@@ -0,0 +1,6 @@
+import coremltools as ct
+
+
+class Converter:
+    def __init__(self):
+        pass

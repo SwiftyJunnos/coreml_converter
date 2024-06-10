Fixed model to use appropriate precision type

src/coreml_converter/converter.py
@@ -22,18 +22,20 @@ class Converter:
         model.eval()
 
         _example_values = example_inputs.values()
-        _LOGGER.info(f"Tracing: {_example_values}")
-        traced_model = jit.trace(model, tuple(_example_values))
+        traced_model = jit.trace(model, list(_example_values))
 
         _LOGGER.info("Start converting...")
+
         converted_model = ct.convert(
             model=traced_model,
             inputs=[
                 ct.TensorType(name=m[0], shape=m[1].shape, dtype=np.int32)
                 for m in example_inputs.items()
             ],
+            outputs=[ct.TensorType(name="output")],
             minimum_deployment_target=ct.target.iOS15,
             convert_to="mlprogram",
+            compute_precision=ct.precision.FLOAT32,
         )
 
         if isinstance(converted_model, MLModel):

src/coreml_converter/models/intent_model.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 from transformers import RobertaModel
 
@@ -11,7 +12,7 @@ class IntentModel(nn.Module):
         self.classifier = nn.Linear(3 * config.hidden_size, num_labels)
         self.sigmoid = nn.Sigmoid()
 
-    def forward(self, input_ids, attention_mask):
+    def forward(self, input_ids: torch.Tensor, attention_mask: torch.Tensor):
         # input_ids.shape: (batch size, 1536)
         batch_size = input_ids.size(0)
 

src/coreml_converter/models/models.py
@@ -1,10 +1,20 @@
+import os
 from enum import StrEnum
 
+import coremltools as ct
 import torch
 import torch.nn as nn
+from coremltools.models import MLModel
 from transformers import RobertaConfig, RobertaTokenizerFast
 
-from .const import G_STEP_NUM_LABEL, KEYWORD_NUM_LABEL, INTENT_NUM_LABEL, MODEL_URL
+from .const import (
+    G_STEP_NUM_LABEL,
+    KEYWORD_NUM_LABEL,
+    INTENT_NUM_LABEL,
+    MODEL_URL,
+    RESULT_DIR,
+    _MLPACKAGE_EXTENSION,
+)
 from .available_models import IntentModel, KeywordModel, GStepModel
 
 
@@ -43,22 +53,22 @@ class Models(StrEnum):
         if self is Models.INTENT:
             _input_size = (input_size[0], input_size[1] * 3)
             return {
-                "input_ids": torch.rand(_input_size).to(torch.int64),
-                "attention_mask": torch.zeros(_input_size).to(torch.int64),
+                "input_ids": torch.ones(_input_size).to(torch.int32),
+                "attention_mask": torch.zeros(_input_size).to(torch.int32),
             }
         elif self == Models.KEYWORD:
             return {
-                "input_ids": torch.rand(input_size).to(torch.int64),
-                "attention_mask": torch.zeros(input_size).to(torch.int64),
+                "input_ids": torch.ones(input_size).to(torch.int32),
+                "attention_mask": torch.zeros(input_size).to(torch.int32),
             }
         elif self == Models.GSTEP:
             return {
-                "doc_input_ids": torch.rand(input_size).to(torch.int64),
-                "doc_attention_mask": torch.zeros(input_size).to(torch.int64),
-                "intent_input_ids": torch.rand(input_size).to(torch.int64),
-                "intent_attention_mask": torch.zeros(input_size).to(torch.int64),
-                "keyword_input_ids": torch.rand(input_size).to(torch.int64),
-                "keyword_attention_mask": torch.zeros(input_size).to(torch.int64),
+                "doc_input_ids": torch.ones(input_size).to(torch.int32),
+                "doc_attention_mask": torch.zeros(input_size).to(torch.int32),
+                "intent_input_ids": torch.ones(input_size).to(torch.int32),
+                "intent_attention_mask": torch.zeros(input_size).to(torch.int32),
+                "keyword_input_ids": torch.ones(input_size).to(torch.int32),
+                "keyword_attention_mask": torch.zeros(input_size).to(torch.int32),
             }
         else:
             raise ValueError("Invalid case for enum `Models`.")
@@ -74,3 +84,9 @@ class Models(StrEnum):
             return GStepModel(config, num_labels)
         else:
             raise ValueError("Invalid case for enum `Models`.")
+
+    def load_coreml_package(self) -> MLModel:
+        model_path = os.path.join(
+            RESULT_DIR, "opt_" + self.value + _MLPACKAGE_EXTENSION
+        )
+        return ct.models.MLModel(model_path)